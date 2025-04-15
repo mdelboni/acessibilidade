680169f91e9bcf0b6849957c323ff4af3e0d393a
mdelboni <maria.delboni.miranda@escola.pr.gov.br>
2025-04-15 09:57:15 -0300

    js

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-    })
-})
+    });
+
+});
+
 
