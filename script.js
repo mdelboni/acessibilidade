e2871285d7079129a04b97d18098e585d5a5fbb4
mdelboni <maria.delboni.miranda@escola.pr.gov.br>
2025-04-24 11:59:08 -0300

    lista

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -25,5 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 });
+
+alternaContraste.addEventListener('click', function () {
+    document.body.classList.toggle('alto-contraste')
+})
+
 
 
